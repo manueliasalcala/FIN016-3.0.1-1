98107e9943137d585c12254348cdb74c2b653619
Manuel Elias Rodriguez Romero <manu@manurdgz.com>
2025-10-27 18:13:30 -0600

    Clase 2025-10-27

--- Q1.R
+++ Q1.R
@@ -7,4 +7,9 @@ data <- pdata.frame(data, index = c("Fecha", "CORP"))
 #To get the answer to the first point in the question, we can simply get the data frame's head and copy the results.
 head(data)
 
+#We use the plm function to run the model.
 pool <- plm(Invest ~ Value + Capital, data = data, model = "pooling")
+summary(pool)
+
+#And save the residuals.
+pool_resid <- residuals(pool)
