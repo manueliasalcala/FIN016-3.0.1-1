3614685df3229f4dd908067c10a91ce5488214ff
Manuel Elias Rodriguez Romero <manu@manurdgz.com>
2025-10-27 18:12:13 -0600

    Clase 2025-10-27

--- Q1.R
+++ Q1.R
@@ -4,4 +4,7 @@ library(plm)
 data <- read_excel("DATAUSS.xlsx")
 data <- pdata.frame(data, index = c("Fecha", "CORP"))
 
+#To get the answer to the first point in the question, we can simply get the data frame's head and copy the results.
+head(data)
+
 pool <- plm(Invest ~ Value + Capital, data = data, model = "pooling")
